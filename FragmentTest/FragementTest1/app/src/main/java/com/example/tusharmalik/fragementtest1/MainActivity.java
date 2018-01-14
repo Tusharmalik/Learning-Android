3e628b0dc62869ecaecaa902ca461e9f9d5713e8
Tusharmalik <itusharmalik62@gmail.com>
2018-01-14 10:21:25 +0530

    made some changes in colors.xml,styles.xml,activity_main.xml and added a menu_main.xml file to make the app look good. Also added some functionality in MainActivity.java file to open settings opions from the app

--- FragmentTest/FragementTest1/app/src/main/java/com/example/tusharmalik/fragementtest1/MainActivity.java
+++ FragmentTest/FragementTest1/app/src/main/java/com/example/tusharmalik/fragementtest1/MainActivity.java
@@ -4,17 +4,28 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button analogButton, digitalButton;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+        getActionBar().setTitle("Clock");
+        toolbar.setSubtitle("tells the time");
+        toolbar.setLogo(R.drawable.clock_logo);
 
         analogButton = findViewById(R.id.button_analog);
         analogButton.setOnClickListener(this);
@@ -23,6 +34,33 @@ public class MainActivity extends Activity implements View.OnClickListener {
         digitalButton.setOnClickListener(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Fragment fragment;
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.menu_date:
+                startActivity(new Intent(android.provider.Settings
+                        .ACTION_DATE_SETTINGS));
+                break;
+            case R.id.menu_location:
+                startActivity(new Intent(android.provider.Settings
+                        .ACTION_LOCALE_SETTINGS));
+                break;
+            case R.id.menu_sleep:
+                startActivity(new Intent(android.provider.Settings
+                        .ACTION_SOUND_SETTINGS));
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onClick(View view) {
